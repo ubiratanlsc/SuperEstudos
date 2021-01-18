7ebf053a609a36b9da541206f71b66ccdbfdfa9e
Ubiratan Lima <ubiratanlsc@gmail.com>
2021-01-18 13:46:09 -0300

    primeiro commit
    ultimo arquivo criado, Controller

---
+++ Controller.js
@@ -0,0 +1,12 @@
+const express=require('express');
+const cors=require('cors');
+const bodyParser=require('body-parser');
+
+const app=express();
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false}));
+
+let port=process.env.PORT || 3000;
+app.listen(port, (req, res)=>{
+    console.log('Server Ok');
+});
